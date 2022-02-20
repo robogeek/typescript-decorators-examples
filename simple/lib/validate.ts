e17cfd0cc2c618f301160f3580cb60a2df638d30
David Herron <david@davidherron.com>
2022-02-20 12:02:58 -0800

    Edits

--- simple/lib/validate.ts
+++ simple/lib/validate.ts
@@ -1,15 +1,41 @@
 
 import * as inspectors from 'decorator-inspectors';
-import * as validators from 'runtime-validators';
+import {
+    accessors, params, ValidateParams
+} from 'runtime-data-validation';
 
 class ToValidate {
 
+    #ratio: number;
+    #year: number;
+
+    @accessors.AccessorValidation<number>()
+    @accessors.IsIntRange(1990, 2050)
+    @accessors.IsInt()
+    set year(ny: number) { 
+        console.log(`validate.ts set year ${ny}`);
+        this.#year = ny; }
+    get year() {
+        console.log(`validate.ts get year ${this.#year}`);
+        return this.#year; }
+    
+    @accessors.AccessorValidation<number>()
+    @accessors.IsFloatRange(0.2, 0.8)
+    @accessors.IsFloat()
+    set ratio(nr: number) {
+        console.log(`validate.ts set ratio ${nr}`);
+        this.#ratio = nr; }
+    get ratio() {
+        console.log(`validate.ts get ratio ${this.#ratio}`);
+        return this.#ratio; }
+    
+
     @inspectors.LogMethodInspector
-    @validators.ValidateParams
+    @ValidateParams
     isGoodSpeed(
         @inspectors.LogParameterInspector
-        @validators.NumberRange(10, 100)
-        @validators.IsInteger()
+        @params.IsIntRange(10, 100)
+        @params.IsInt()
         speed: number) {
 
         console.log(`IsGoodSpeed ${speed}`);
@@ -20,5 +46,16 @@ const tv = new ToValidate();
 
 tv.isGoodSpeed(15);
 tv.isGoodSpeed(20);
-tv.isGoodSpeed(20.555);
-tv.isGoodSpeed(5);
+// tv.isGoodSpeed(20.555);
+// tv.isGoodSpeed(5);
+
+tv.year = 1990;
+tv.year = 2000;
+tv.year = 2020;
+// tv.year = 2060;
+// tv.year = 1980;
+
+tv.ratio = 0.25;
+tv.ratio = 0.55;
+// tv.ratio = 0.05;
+// tv.ratio = 0.95;
